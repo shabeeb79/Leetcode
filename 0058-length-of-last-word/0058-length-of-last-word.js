d4f76ae7883c4351b7912ea31b02f6974a5e4f1a
shabeeb79 <mshabeeb7912@gmail.com>
2025-10-10 09:27:11 +0530

    Time: 0 ms (100%), Space: 53.9 MB (54.63%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    let words = s.trim().split(" ")
+    let word =""
+     for(i=0;i<words.length;i++){
+       word =  words[words.length-1]
+        
+     }
+    return word.length  
+};
