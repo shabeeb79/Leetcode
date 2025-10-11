4b86e13281361ae0a807c862cf59f5daf570172e
shabeeb79 <mshabeeb7912@gmail.com>
2025-10-11 15:42:55 +0530

    Time: 0 ms (100%), Space: 54.5 MB (74.11%) - LeetHub

--- 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.js
+++ 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.js
@@ -5,11 +5,11 @@
 var finalValueAfterOperations = function(operations) {
     let x =0
     for(i=0;i<operations.length;i++){
-        if(operations[i] === "--X" || operations[i] === "X--" ){
-            x -= 1
-        }else if(operations[i] === "++X" || operations[i] === "X++" ){
-            x +=1
-        }
+       if(operations[i].includes("--")){
+        x--
+       }else{
+        x++
+       }
         
     }
     return x
