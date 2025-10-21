c4f002df35bd5e429591c5a65bf9c037caf169da
mohammed shabeeb vk <mashabeeb7912@gmail.com>
2025-10-21 21:42:48 +0530

    ##########

---
+++ sample.js
@@ -0,0 +1,14 @@
+var leftRightDifference = function(nums) {
+    for(i=0;i<nums.length-1;i++)
+    {
+    console.log(nums[i]);
+    
+    }
+      for(j=nums.length-1;j>0;j--)
+    {
+      console.log(nums[j]);  
+    }
+};
+console.log(leftRightDifference([10,4,8,3]));
+
+
