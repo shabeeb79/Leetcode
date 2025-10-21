ef46715d5e48ffaa731fe0eb6ce2c8b6889bc122
shabeeb79 <mshabeeb7912@gmail.com>
2025-10-21 21:59:54 +0530

    Time: 68 ms (13.1%), Space: 66.8 MB (94.42%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+ nums.sort((a,b)=> a-b)
+ for(let i=0;i<nums.length;i++){
+ if(nums[i]===nums[i+1])
+    return true;
+ 
+ }
+ return false
+}
+console.log(containsDuplicate([1,2,3,22,22]))
+console.log(containsDuplicate([1]))
