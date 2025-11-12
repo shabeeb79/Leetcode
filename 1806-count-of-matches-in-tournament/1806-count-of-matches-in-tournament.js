16d8aaf7323e624d0af1ca07be3fe8fc0bc19b8f
Shabeeb <mshabeeb7912@gmail.com>
2025-11-12 09:51:01 +0530

    Time: 59 ms (6.17%), Space: 54.7 MB (11.11%) - LeetHub

--- 1806-count-of-matches-in-tournament/1806-count-of-matches-in-tournament.js
+++ 1806-count-of-matches-in-tournament/1806-count-of-matches-in-tournament.js
@@ -3,5 +3,17 @@
  * @return {number}
  */
 var numberOfMatches = function(n) {
-    return n-1
+    let matches = 0;
+    
+    while (n > 1) {
+        if (n % 2 === 0) {
+            matches += n / 2;
+            n = n / 2;
+        } else {
+            matches += (n - 1) / 2;
+            n = (n - 1) / 2 + 1;
+        }
+    }
+    
+    return matches;
 };
