ed2dedc9c9061eaf7ed52e9d848024e224d21996
Shabeeb <mshabeeb7912@gmail.com>
2025-12-09 20:24:14 +0530

    Time: 0 ms (100%), Space: 53.4 MB (88.84%) - LeetHub

---
+++ 2520-count-the-digits-that-divide-a-number/2520-count-the-digits-that-divide-a-number.js
@@ -0,0 +1,11 @@
+var countDigits = function(num) {
+    let count = 0
+  let arr = [num].join('').split('').map(Number)
+    for(i=0;i<arr.length;i++){
+        if(num % arr[i] == 0){
+          count++
+        }
+        
+    }
+    return count
+};
