25bf8cc703ece40d086e8d5ac3d488c923619312
Shabeeb <mshabeeb7912@gmail.com>
2025-12-11 23:20:58 +0530

    Time: 0 ms (100%), Space: 55.7 MB (51.76%) - LeetHub

---
+++ 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} hours
+ * @param {number} target
+ * @return {number}
+ */
+var numberOfEmployeesWhoMetTarget = function(hours, target) {
+    let count = 0
+    for(i=0;i<hours.length;i++){
+        if(hours[i]>=target){
+             count++
+        }
+       
+    }
+     return count
+};
