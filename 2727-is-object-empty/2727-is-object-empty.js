820d124641e3974e170c7cf5bc6666bd285606e8
Shabeeb <mshabeeb7912@gmail.com>
2026-01-13 20:41:59 +0530

    Time: 43 ms (67.18%), Space: 53.7 MB (90.86%) - LeetHub

---
+++ 2727-is-object-empty/2727-is-object-empty.js
@@ -0,0 +1,17 @@
+/**
+ * @param {Object|Array} obj
+ * @return {boolean}
+ */
+var isEmpty = function(obj) {
+   if (JSON.stringify(obj).length<=2){
+    return true;
+   }else{
+    return false
+   }
+
+
+   
+}
+
+
+isEmpty({ });
